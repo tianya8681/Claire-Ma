701164f658d2f89be57d0c41ec4b62ac598e06eb
tianya8681 <63326119+tianya8681@users.noreply.github.com>
2023-10-07 09:15:57 +0800

    Update functions.js
    修改时间

--- Love_files/functions.js
+++ Love_files/functions.js
@@ -40,18 +40,18 @@ $(window).resize(function() {
 function timeElapse(date){
 	var current = Date();
 	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
-	var days = Math.floor(seconds / (3600 * 24));
+	var days = 134
 	seconds = seconds % (3600 * 24);
-	var hours = Math.floor(seconds / 3600);
+	var hours = 22
 	if (hours < 10) {
 		hours = "0" + hours;
 	}
 	seconds = seconds % 3600;
-	var minutes = Math.floor(seconds / 60);
+	var minutes = 02
 	if (minutes < 10) {
 		minutes = "0" + minutes;
 	}
-	seconds = seconds % 60;
+	seconds = 23
 	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
