570d65e7319c4efae00ac64a06b567d99c6fdbe6
tianya8681 <63326119+tianya8681@users.noreply.github.com>
2023-10-07 09:32:03 +0800

    Update functions.js

--- Love_files/functions.js
+++ Love_files/functions.js
@@ -60,6 +60,6 @@ function timeElapse(date){
 		seconds = "0" + seconds;
 	}
 	//var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒";
-	var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒";
+	var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒：时间停止了~！";
 	$("#clock").html(result);
 }
