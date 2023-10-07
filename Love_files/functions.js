c04572bd8128f3178853ecc3729761e680067668
tianya8681 <63326119+tianya8681@users.noreply.github.com>
2023-10-07 09:20:01 +0800

    Update functions.js
    直接修改时间

--- Love_files/functions.js
+++ Love_files/functions.js
@@ -55,6 +55,6 @@ function timeElapse(date){
 	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
-	var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒"; 
+	var result = "第 <span class=\"digit\">" +134 + "</span> 天 <span class=\"digit\">" + 22 + "</span> 小时 <span class=\"digit\">" + 02 + "</span> 分钟 <span class=\"digit\">" + 23 + "</span> 秒"; 
 	$("#clock").html(result);
 }
