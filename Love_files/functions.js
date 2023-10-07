1c9fc146c2e0e3e777a9b4103a5678b4cb0eaf7e
tianya8681 <63326119+tianya8681@users.noreply.github.com>
2023-10-07 09:28:46 +0800

    Update functions.js
    替換 函數

--- Love_files/functions.js
+++ Love_files/functions.js
@@ -40,21 +40,26 @@ $(window).resize(function() {
 function timeElapse(date){
 	var current = Date();
 	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+	//var days = Math.floor(seconds / (3600 * 24));
 	var days = 134
 	seconds = seconds % (3600 * 24);
+	//var hours = Math.floor(seconds / 3600);
 	var hours = 22
 	if (hours < 10) {
 		hours = "0" + hours;
 	}
 	seconds = seconds % 3600;
+	//var minutes = Math.floor(seconds / 60);
 	var minutes = 02
 	if (minutes < 10) {
 		minutes = "0" + minutes;
 	}
+	//seconds = seconds % 60;
 	seconds = 23
 	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
-	var result = "第 <span class=\"digit\">" +134 + "</span> 天 <span class=\"digit\">" + 22 + "</span> 小时 <span class=\"digit\">" + 02 + "</span> 分钟 <span class=\"digit\">" + 23 + "</span> 秒"; 
+	//var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒";
+	var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒";
 	$("#clock").html(result);
 }
